c95a07cd3316660eba2042a5fefdbbc113ea7592
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-06 11:32:43 -0700

    Implement new pint and pop.

---
+++ pop_monty.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * mpop - removes the top element of the stack
+ * @h: head
+ * @count: line number
+ *
+ * Return: nothing.
+ */
+void mpop(stack_t **h, unsigned int count)
+{
+	stack_t *temp;
+
+	if (*h == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an stack empty\n", count);
+		fclose(variable.file);
+		free(variable.text);
+		free_stack(*h);
+		exit(EXIT_FAILURE);
+	}
+	temp = *h;
+	*h = temp->next;
+	free(temp);
+}
